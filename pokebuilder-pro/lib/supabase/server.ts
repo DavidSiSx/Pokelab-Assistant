0e0e8cb5b19c742707bd41a832abfbce584df52a
DavidSiSx <davidsierrasosa01@gmail.com>
2026-03-03 02:48:51 -0500

    server.ts cambio de variables

--- pokebuilder-pro/lib/supabase/server.ts
+++ pokebuilder-pro/lib/supabase/server.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+// lib/supabase/server.ts
 export function createServerClient() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // Changed from SERVICE_ROLE_KEY
   );
 }
